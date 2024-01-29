3112457aaac55596663ba945053e0cf988dfd086
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-01-29 21:39:46 +0530

    Add files via upload

---
+++ Bubblesort.java
@@ -0,0 +1,27 @@
+public class Bubblesort {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 16, 8, 9, 5 };
+
+        // bubble sort
+        // time complexity = O(n^2)
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
